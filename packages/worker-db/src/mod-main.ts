aa82b258d983320244475356db42c1370929341d
Ademar Arvati Filho <arvati@hotmail.com>
2026-09-19 10:35:46 -0300

    exportação de versão e doc --lint

--- packages/worker-db/src/mod-main.ts
+++ packages/worker-db/src/mod-main.ts
@@ -5,6 +5,7 @@ export type { WorkerLsAPI } from "./ls.ts";
 export { db as dbsw, opfs as opfssw } from "./db.ts";
 export { db, opfs } from "./rpc.ts";
 export { gerarId, gerarIdComPrefixo, validarId } from "./utils/id.ts";
+export { APP_VERSION as version } from "./utils/version.ts";
 export type {
   DbStoreOptions,
   IndexQuery,
--- packages/worker-db/src/mod-sw.ts
+++ packages/worker-db/src/mod-sw.ts
@@ -2,6 +2,7 @@
 
 export { db, opfs } from "./db.ts";
 export { gerarId, gerarIdComPrefixo, validarId } from "./utils/id.ts";
+export { APP_VERSION as version } from "./utils/version.ts";
 export type {
   DbStoreOptions,
   IndexQuery,
