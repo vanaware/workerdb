637de674f30f727caa04f69b131ca99f40733ab2
Ademar Arvati <arvati@hotmail.com>
2026-09-19 14:18:37 -0300

    build: update project version and add dependencies
    Bump project version to 0.1.5 and include additional @std library dependencies in the lockfile to support project requirements.

--- packages/worker-db/src/utils/version.ts
+++ packages/worker-db/src/utils/version.ts
@@ -4,4 +4,4 @@ declare const __APP_VERSION__: string;
 /** Versão atual da aplicação. */
 export const APP_VERSION: string = typeof __APP_VERSION__ !== "undefined"
   ? __APP_VERSION__
-  : "0.1.2-mu8n3ao4";
+  : "0.1.5-mu8ngpsk";
