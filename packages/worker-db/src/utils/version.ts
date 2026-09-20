75610a4c6372a4a1e359756d594e25de0f32f84d
Ademar Arvati Filho <arvati@hotmail.com>
2026-09-20 17:31:18 -0300

    Versão v0.3

--- packages/worker-db/src/utils/version.ts
+++ packages/worker-db/src/utils/version.ts
@@ -4,4 +4,4 @@ declare const __APP_VERSION__: string;
 /** Current library/application version. */
 export const APP_VERSION: string = typeof __APP_VERSION__ !== "undefined"
   ? __APP_VERSION__
-  : "1.0.3#h3";
+  : "0.3.0#mua9rvo8";
